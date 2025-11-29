6fd89bf94473b4a13defb42da76919129719d57a
ranganathantvb <rangan.nath@gmail.com>
2025-11-29 19:42:21 +0530

    My Initial Commit

---
+++ src/main/java/com/codepulse/catelog/CatelogApplication.java
@@ -0,0 +1,13 @@
+package com.codepulse.catelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatelogApplication.class, args);
+	}
+
+}
