6414aa22d519af239e9c1a242cb75ff672c72aec
ranganathantvb <rangan.nath@gmail.com>
2026-01-17 12:51:54 +0530

    Eval sample 10: java:S5445 (temp file)

---
+++ src/main/java/com/svc_catalog/demo/insecure/DemoInsecureController10.java
@@ -0,0 +1,21 @@
+package com.svc_catalog.demo.insecure;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo/insecure")
+@ConditionalOnProperty(prefix = "demo.insecure", name = "enabled", havingValue = "true")
+public class DemoInsecureController10 {
+
+    @GetMapping("/s5445")
+    public String s5445() throws IOException {
+        File tempFile = File.createTempFile("demo", ".tmp");
+        return "ok:" + tempFile.getAbsolutePath();
+    }
+}
