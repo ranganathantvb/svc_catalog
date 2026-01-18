dcf2293603bad71ae8d7402d19d8cc971dfc7b8e
ranganathantvb <rangan.nath@gmail.com>
2026-01-18 14:00:52 +0530

    test(sonar): add sample for java:S1118

---
+++ src/main/java/com/svc_catalog/sonarsamples/PublicUtilityConstructor.java
@@ -0,0 +1,10 @@
+package com.svc_catalog.sonarsamples;
+
+public class PublicUtilityConstructor {
+    public PublicUtilityConstructor() {
+    }
+
+    public static String label(String value) {
+        return "value:" + value;
+    }
+}
