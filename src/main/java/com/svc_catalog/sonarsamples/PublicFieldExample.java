9e5db476525e82ba19e8c481c675b96f37641e14
ranganathantvb <rangan.nath@gmail.com>
2026-02-09 15:37:58 +0530

    Fix java:S1104 (CODE_SMELL): src/main/java/com/svc_catalog/sonarsamples/PublicFieldExample.java (CodePulse) [AZvQSfXpQS0Yxw0VjxrY]

--- src/main/java/com/svc_catalog/sonarsamples/PublicFieldExample.java
+++ src/main/java/com/svc_catalog/sonarsamples/PublicFieldExample.java
@@ -1,5 +1,11 @@
 package com.svc_catalog.sonarsamples;
 
 public class PublicFieldExample {
-    public String name = "sample";
+    private String name; 
+ public String getName() {
+ return this.name;
+} 
+ public void setName(String name) {
+ this.name = name;
+}
 }
